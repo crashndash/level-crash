4d00b9472f3df955da2a7b94f87ceeb5cce7b779
Eirik S. Morland <eirik@morland.no>
2014-04-19 19:02:04 +0200

    Add startup script. Fix #3Add startup script. Fix #3..

--- src/app.js
+++ src/app.js
@@ -54,11 +54,11 @@ app.log = function(str, severity) {
 
 // Read app config.
 try {
-  app.config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
+  app.config = yaml.safeLoad(fs.readFileSync(__dirname + '/../config.yml', 'utf8'));
 }
 catch(err) {
   app.log('No config.yml file found. Using default.config.yml for now. This is most likely not what you want to do.', 'w');
-  app.config = yaml.safeLoad(fs.readFileSync('./default.config.yml', 'utf8'));
+  app.config = yaml.safeLoad(fs.readFileSync(__dirname + '/../default.config.yml', 'utf8'));
 }
 var auth = require('../lib/auth');
 
