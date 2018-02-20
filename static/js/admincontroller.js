10ef7512f28cf5ad533e4667908c4fd89b02f013
Eirik S. Morland <eirik@morland.no>
2018-02-20 16:14:05 +0100

    Fix standard coding style for newest version

--- static/js/admincontroller.js
+++ static/js/admincontroller.js
@@ -1,36 +1,36 @@
 ;(function (angular) {
   angular.module('levelAdmin', [])
-  .controller('adminCtrl', function ($scope, $http) {
-    var loadLevels = function () {
-      $http.get('/api/level')
-      .success(function (d) {
-        $scope.levels = d
-      })
-    }
-    loadLevels()
-    $scope.deleteLevel = function (level) {
-      if (window.confirm('Are you sure you want to delete the level' + level + '?')) {
-        $http.delete('/admin/level/' + level)
-        .success(function (d) {
-          loadLevels()
-        })
+    .controller('adminCtrl', function ($scope, $http) {
+      var loadLevels = function () {
+        $http.get('/api/level')
+          .success(function (d) {
+            $scope.levels = d
+          })
       }
-    }
-    $scope.loadLevel = function (level) {
-      window.location.href = '/level/' + level
-    }
-    $scope.predicate = function (level) {
-      if (!level.timestamp) {
-        return 0
+      loadLevels()
+      $scope.deleteLevel = function (level) {
+        if (window.confirm('Are you sure you want to delete the level' + level + '?')) {
+          $http.delete('/admin/level/' + level)
+            .success(function (d) {
+              loadLevels()
+            })
+        }
       }
-      return level.timestamp
-    }
-    $scope.creation = function (time) {
-      var d = new Date(time)
-      if (d.toUTCString() === 'Invalid Date') {
-        return 'Unknown'
+      $scope.loadLevel = function (level) {
+        window.location.href = '/level/' + level
       }
-      return d.toUTCString()
-    }
-  })
+      $scope.predicate = function (level) {
+        if (!level.timestamp) {
+          return 0
+        }
+        return level.timestamp
+      }
+      $scope.creation = function (time) {
+        var d = new Date(time)
+        if (d.toUTCString() === 'Invalid Date') {
+          return 'Unknown'
+        }
+        return d.toUTCString()
+      }
+    })
 }(window.angular))
